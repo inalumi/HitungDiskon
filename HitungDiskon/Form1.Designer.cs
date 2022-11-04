d213c7cfeb676405f9f2056b213d0c3c07702250
inalumi <rhi.becks@gmail.com>
2022-11-04 15:53:23 +0700

    Add files via upload

---
+++ HitungDiskon/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace HitungDiskon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dgvUOM = new System.Windows.Forms.DataGridView();
+            this.btnSaveUOM = new System.Windows.Forms.Button();
+            this.txtItemDesc = new System.Windows.Forms.TextBox();
+            this.txtQty = new System.Windows.Forms.TextBox();
+            this.txtUnitPrice = new System.Windows.Forms.TextBox();
+            this.txtDiscount = new System.Windows.Forms.TextBox();
+            this.txtDiscountAmt = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.cmbUOM = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtRate = new System.Windows.Forms.TextBox();
+            this.txtQtyKecil = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUOM)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.txtQtyKecil);
+            this.groupBox1.Controls.Add(this.txtRate);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cmbUOM);
+            this.groupBox1.Controls.Add(this.txtTotal);
+            this.groupBox1.Controls.Add(this.txtDiscountAmt);
+            this.groupBox1.Controls.Add(this.txtDiscount);
+            this.groupBox1.Controls.Add(this.txtUnitPrice);
+            this.groupBox1.Controls.Add(this.txtQty);
+            this.groupBox1.Controls.Add(this.txtItemDesc);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(488, 397);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnSaveUOM);
+            this.groupBox2.Controls.Add(this.dgvUOM);
+            this.groupBox2.Location = new System.Drawing.Point(506, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(398, 397);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Tabel UOM";
+            // 
+            // dgvUOM
+            // 
+            this.dgvUOM.AllowUserToAddRows = false;
+            this.dgvUOM.AllowUserToDeleteRows = false;
+            this.dgvUOM.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUOM.Location = new System.Drawing.Point(15, 33);
+            this.dgvUOM.Name = "dgvUOM";
+            this.dgvUOM.RowTemplate.Height = 24;
+            this.dgvUOM.Size = new System.Drawing.Size(377, 164);
+            this.dgvUOM.TabIndex = 0;
+            this.dgvUOM.EditingControlShowing += new System.Windows.Forms.DataGridViewEditingControlShowingEventHandler(this.dgvUOM_EditingControlShowing);
+            // 
+            // btnSaveUOM
+            // 
+            this.btnSaveUOM.Location = new System.Drawing.Point(15, 208);
+            this.btnSaveUOM.Name = "btnSaveUOM";
+            this.btnSaveUOM.Size = new System.Drawing.Size(93, 36);
+            this.btnSaveUOM.TabIndex = 1;
+            this.btnSaveUOM.Text = "Save UOM";
+            this.btnSaveUOM.UseVisualStyleBackColor = true;
+            this.btnSaveUOM.Click += new System.EventHandler(this.btnSaveUOM_Click);
+            // 
+            // txtItemDesc
+            // 
+            this.txtItemDesc.Location = new System.Drawing.Point(99, 33);
+            this.txtItemDesc.Name = "txtItemDesc";
+            this.txtItemDesc.Size = new System.Drawing.Size(121, 22);
+            this.txtItemDesc.TabIndex = 0;
+            // 
+            // txtQty
+            // 
+            this.txtQty.Location = new System.Drawing.Point(99, 91);
+            this.txtQty.Name = "txtQty";
+            this.txtQty.Size = new System.Drawing.Size(121, 22);
+            this.txtQty.TabIndex = 1;
+            this.txtQty.TextChanged += new System.EventHandler(this.txtQty_TextChanged);
+            this.txtQty.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtQty_KeyPress);
+            // 
+            // txtUnitPrice
+            // 
+            this.txtUnitPrice.Location = new System.Drawing.Point(99, 119);
+            this.txtUnitPrice.Name = "txtUnitPrice";
+            this.txtUnitPrice.Size = new System.Drawing.Size(121, 22);
+            this.txtUnitPrice.TabIndex = 2;
+            this.txtUnitPrice.TextChanged += new System.EventHandler(this.txtUnitPrice_TextChanged);
+            this.txtUnitPrice.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtUnitPrice_KeyPress);
+            // 
+            // txtDiscount
+            // 
+            this.txtDiscount.Location = new System.Drawing.Point(99, 147);
+            this.txtDiscount.Name = "txtDiscount";
+            this.txtDiscount.Size = new System.Drawing.Size(121, 22);
+            this.txtDiscount.TabIndex = 3;
+            this.txtDiscount.TextChanged += new System.EventHandler(this.txtDiscount_TextChanged);
+            // 
+            // txtDiscountAmt
+            // 
+            this.txtDiscountAmt.Location = new System.Drawing.Point(99, 175);
+            this.txtDiscountAmt.Name = "txtDiscountAmt";
+            this.txtDiscountAmt.ReadOnly = true;
+            this.txtDiscountAmt.Size = new System.Drawing.Size(121, 22);
+            this.txtDiscountAmt.TabIndex = 4;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(99, 203);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.ReadOnly = true;
+            this.txtTotal.Size = new System.Drawing.Size(121, 22);
+            this.txtTotal.TabIndex = 5;
+            // 
+            // cmbUOM
+            // 
+            this.cmbUOM.FormattingEnabled = true;
+            this.cmbUOM.Location = new System.Drawing.Point(99, 61);
+            this.cmbUOM.Name = "cmbUOM";
+            this.cmbUOM.Size = new System.Drawing.Size(121, 24);
+            this.cmbUOM.TabIndex = 6;
+            this.cmbUOM.SelectedIndexChanged += new System.EventHandler(this.cmbUOM_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Item Desc";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "UOM";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Qty";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "UnitPrice";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 152);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Discount";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 180);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(87, 17);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "DiscountAmt";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 208);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 17);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Total";
+            // 
+            // txtRate
+            // 
+            this.txtRate.Location = new System.Drawing.Point(305, 61);
+            this.txtRate.Name = "txtRate";
+            this.txtRate.ReadOnly = true;
+            this.txtRate.Size = new System.Drawing.Size(121, 22);
+            this.txtRate.TabIndex = 14;
+            // 
+            // txtQtyKecil
+            // 
+            this.txtQtyKecil.Location = new System.Drawing.Point(303, 91);
+            this.txtQtyKecil.Name = "txtQtyKecil";
+            this.txtQtyKecil.ReadOnly = true;
+            this.txtQtyKecil.Size = new System.Drawing.Size(121, 22);
+            this.txtQtyKecil.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(240, 66);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(33, 17);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "rate";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(229, 93);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(64, 17);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Qty Kecil";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1009, 587);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUOM)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtQtyKecil;
+        private System.Windows.Forms.TextBox txtRate;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbUOM;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.TextBox txtDiscountAmt;
+        private System.Windows.Forms.TextBox txtDiscount;
+        private System.Windows.Forms.TextBox txtUnitPrice;
+        private System.Windows.Forms.TextBox txtQty;
+        private System.Windows.Forms.TextBox txtItemDesc;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnSaveUOM;
+        private System.Windows.Forms.DataGridView dgvUOM;
+    }
+}
+
---
+++ HitungDiskon/Form1.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HitungDiskon
+{
+    public partial class Form1 : Form
+    {
+        private DataTable dtsource;
+        public Form1()
+        {
+            InitializeComponent();
+            loadGridAwal();
+            bindCmbUOM();
+            loadAwal();
+        }
+
+        private void loadAwal()
+        {
+            txtDiscount.Text = "";
+            txtDiscountAmt.Text = "";
+            txtItemDesc.Text = "";
+            txtQty.Text = "";
+            txtQtyKecil.Text = "";
+            txtTotal.Text = "";
+            txtUnitPrice.Text = "";
+            cmbUOM.SelectedIndex = 0;
+
+            txtRate.Text = cmbUOM.SelectedValue.ToString();
+
+        }
+
+        private void bindCmbUOM()
+        {
+            cmbUOM.DataSource = dgvUOM.DataSource;
+            cmbUOM.DisplayMember = "Satuan";
+            cmbUOM.ValueMember = "Rate";
+            cmbUOM.SelectedIndex = 0;
+
+            txtRate.Text = cmbUOM.SelectedValue.ToString();
+        }
+
+        private void loadGridAwal()
+        {
+            dtsource = new DataTable();
+            dtsource.Columns.Add("Satuan", typeof(string));
+            dtsource.Columns.Add("Rate", typeof(int));
+            DataRow row = dtsource.NewRow();
+            row[0] = "PCS";
+            row[1] = 1;
+            dtsource.Rows.Add(row);
+            row = dtsource.NewRow();
+            row[0] = "LUSIN";
+            row[1] = 12;
+            dtsource.Rows.Add(row);
+            row = dtsource.NewRow();
+            row[0] = "BOX";
+            row[1] = 24;
+            dtsource.Rows.Add(row);
+            dtsource.AcceptChanges();
+
+            dgvUOM.DataSource = dtsource;
+            dgvUOM.Columns[0].ReadOnly = true;
+        }
+
+        private void btnSaveUOM_Click(object sender, EventArgs e)
+        {
+            dtsource = new DataTable();
+            dtsource.Columns.Add("Satuan", typeof(string));
+            dtsource.Columns.Add("Rate", typeof(int));
+            foreach (DataGridViewRow rowview in dgvUOM.Rows)
+            {
+                if (rowview.Cells[0].ToString() != "" && rowview.Cells[1].ToString() != "")
+                {
+                    DataRow row = dtsource.NewRow();
+                    row[0] = rowview.Cells[0].Value;
+                    row[1] = rowview.Cells[1].Value;
+                    dtsource.Rows.Add(row);
+                    row = dtsource.NewRow();
+                }
+            }
+            dgvUOM.DataSource = null;
+            dgvUOM.DataSource = dtsource;
+            dgvUOM.Columns[0].ReadOnly = true;
+            bindCmbUOM();
+            loadAwal();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvUOM_EditingControlShowing(object sender, DataGridViewEditingControlShowingEventArgs e)
+        {
+            e.Control.KeyPress -= new KeyPressEventHandler(Column1_KeyPress);
+            if (dgvUOM.CurrentCell.ColumnIndex == 1)
+            {
+                TextBox tb = e.Control as TextBox;
+                if (tb != null)
+                {
+                    tb.KeyPress += new KeyPressEventHandler(Column1_KeyPress);
+                }
+            }
+        }
+
+        private void Column1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtQty_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtUnitPrice_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void cmbUOM_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtRate.Text = cmbUOM.SelectedValue.ToString();
+
+            if (txtQty.Text != "")
+            {
+                txtQtyKecil.Text = (Convert.ToInt32(txtQty.Text) * Convert.ToInt32(txtRate.Text)).ToString();
+            }
+        }
+
+        private void txtQty_TextChanged(object sender, EventArgs e)
+        {
+            if (txtQty.Text != "")
+            {
+                txtQtyKecil.Text = (Convert.ToInt32(txtQty.Text) * Convert.ToInt32(txtRate.Text)).ToString();
+                if (txtUnitPrice.Text != "")
+                {
+                    if (txtDiscount.Text != "")
+                    {
+                        if (!txtDiscount.Text.Contains("%") && !txtDiscount.Text.Contains("+"))
+                        {
+                            txtDiscountAmt.Text = txtDiscount.Text;
+                            txtTotal.Text = (Convert.ToInt32(txtQty.Text) * Convert.ToInt32(txtUnitPrice.Text) - Convert.ToInt32(txtDiscount.Text)).ToString();
+                        }
+                        else
+                        {
+                            try
+                            {
+                                if (txtDiscount.Text.Contains("+"))
+                                {
+                                    string dis = txtDiscount.Text.Replace("%", "");
+                                    string[] arrdis = dis.Split('+');
+                                    int dsk = 0;
+                                    for (int i = 0; i < arrdis.Length; i++)
+                                    {
+                                        if (arrdis[i] != "")
+                                        {
+                                            dsk += Convert.ToInt32(arrdis[i]);
+                                        }
+                                    }
+
+                                    txtDiscountAmt.Text = ((Convert.ToInt32(txtQty.Text) * Convert.ToInt32(txtUnitPrice.Text) * dsk) / 100).ToString();
+                                    txtTotal.Text = (Convert.ToInt32(txtQty.Text) * Convert.ToInt32(txtUnitPrice.Text) - Convert.ToInt32(txtDiscountAmt.Text)).ToString();
+                                }
+                                else
+                                {
+                                    int dsk = Convert.ToInt32(txtDiscount.Text.Replace("%", ""));
+
+                                    txtDiscountAmt.Text = ((Convert.ToInt32(txtQty.Text) * Convert.ToInt32(txtUnitPrice.Text) * dsk) / 100).ToString();
+                                    txtTotal.Text = (Convert.ToInt32(txtQty.Text) * Convert.ToInt32(txtUnitPrice.Text) - Convert.ToInt32(txtDiscountAmt.Text)).ToString();
+                                }
+                            }
+                            catch
+                            {
+                                MessageBox.Show("Salah format diskon");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        txtTotal.Text = (Convert.ToInt32(txtQty.Text) * Convert.ToInt32(txtUnitPrice.Text)).ToString();
+                    }
+                }
+            }
+            else
+            {
+                txtQtyKecil.Text = "";
+                txtTotal.Text = "";
+            }
+
+        }
+
+        private void txtUnitPrice_TextChanged(object sender, EventArgs e)
+        {
+            if (txtUnitPrice.Text != "")
+            {
+                if (txtDiscount.Text != "")
+                {
+                    if (txtQty.Text != "")
+                    {
+                        if (!txtDiscount.Text.Contains("%") && !txtDiscount.Text.Contains("+"))
+                        {
+                            txtDiscountAmt.Text = txtDiscount.Text;
+                            txtTotal.Text = (Convert.ToInt32(txtQty.Text) * Convert.ToInt32(txtUnitPrice.Text) - Convert.ToInt32(txtDiscount.Text)).ToString();
+                        }
+                        else
+                        {
+                            try
+                            {
+                                if (txtDiscount.Text.Contains("+"))
+                                {
+                                    string dis = txtDiscount.Text.Replace("%", "");
+                                    string[] arrdis = dis.Split('+');
+                                    int dsk = 0;
+                                    for (int i = 0; i < arrdis.Length; i++)
+                                    {
+                                        if (arrdis[i] != "")
+                                        {
+                                            dsk += Convert.ToInt32(arrdis[i]);
+                                        }
+                                    }
+
+                                    txtDiscountAmt.Text = ((Convert.ToInt32(txtQty.Text) * Convert.ToInt32(txtUnitPrice.Text) * dsk) / 100).ToString();
+                                    txtTotal.Text = (Convert.ToInt32(txtQty.Text) * Convert.ToInt32(txtUnitPrice.Text) - Convert.ToInt32(txtDiscountAmt.Text)).ToString();
+                                }
+                                else
+                                {
+                                    int dsk = Convert.ToInt32(txtDiscount.Text.Replace("%", ""));
+
+                                    txtDiscountAmt.Text = ((Convert.ToInt32(txtQty.Text) * Convert.ToInt32(txtUnitPrice.Text) * dsk) / 100).ToString();
+                                    txtTotal.Text = (Convert.ToInt32(txtQty.Text) * Convert.ToInt32(txtUnitPrice.Text) - Convert.ToInt32(txtDiscountAmt.Text)).ToString();
+                                }
+                            }
+                            catch
+                            {
+                                MessageBox.Show("Salah format diskon");
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    if (txtQty.Text != "")
+                    {
+                        txtTotal.Text = (Convert.ToInt32(txtQty.Text) * Convert.ToInt32(txtUnitPrice.Text)).ToString();
+                    }
+                }
+            }
+            else
+            {
+                txtTotal.Text = "";
+            }
+        }
+
+        private void txtDiscount_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void txtDiscount_TextChanged(object sender, EventArgs e)
+        {
+            if (txtDiscount.Text != "" && txtQty.Text != "" && txtUnitPrice.Text != "")
+            {
+                if (!txtDiscount.Text.Contains("%") && !txtDiscount.Text.Contains("+"))
+                {
+                    txtDiscountAmt.Text = txtDiscount.Text;
+                    try { txtTotal.Text = (Convert.ToInt32(txtQty.Text) * Convert.ToInt32(txtUnitPrice.Text) - Convert.ToInt32(txtDiscount.Text)).ToString(); } catch { MessageBox.Show("Format Salah"); }
+                }
+                else
+                {
+                    try
+                    {
+                        if (txtDiscount.Text.Contains("+"))
+                        {
+                            string dis = txtDiscount.Text.Replace("%","");
+                            string[] arrdis = dis.Split('+');
+                            int dsk = 0;
+                            for (int i = 0; i < arrdis.Length; i++)
+                            {
+                                if (arrdis[i]!="")
+                                {
+                                    dsk += Convert.ToInt32(arrdis[i]);
+                                }
+                            }
+                            
+                            txtDiscountAmt.Text = ((Convert.ToInt32(txtQty.Text) * Convert.ToInt32(txtUnitPrice.Text) * dsk) / 100).ToString();
+                            txtTotal.Text = (Convert.ToInt32(txtQty.Text) * Convert.ToInt32(txtUnitPrice.Text) - Convert.ToInt32(txtDiscountAmt.Text)).ToString();
+                        }
+                        else
+                        {
+                            int dsk = Convert.ToInt32(txtDiscount.Text.Replace("%", ""));
+
+                            txtDiscountAmt.Text = ((Convert.ToInt32(txtQty.Text) * Convert.ToInt32(txtUnitPrice.Text) * dsk) / 100).ToString();
+                            txtTotal.Text = (Convert.ToInt32(txtQty.Text) * Convert.ToInt32(txtUnitPrice.Text) - Convert.ToInt32(txtDiscountAmt.Text)).ToString();
+                        }
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Salah format diskon");
+                    }
+                }
+            }
+        }
+    }
+}
